569abda42a1250071088f96b3bc065cfb85520ee
Ahnbujang <jhpower901@gmail.com>
2024-11-12 11:39:17 +0900

    W5

---
+++ W5/SequenceDetector.v
@@ -0,0 +1,47 @@
+/* detect 0101 sequence in stream
+ * mealym machine
+**/
+module SequenceDetector (
+    input clk, reset_n, d_in,
+    output reg d_out
+);
+    reg [1:0] n_state;       //next state
+    reg [1:0] c_state;       //current state
+
+    //define state
+    localparam RO       = 2'b00;
+    localparam RZ       = 2'b01;
+    localparam Z2O      = 2'b10;
+    localparam Z2O2Z    = 2'b11;
+
+    //Part 1: initialize and update the state register
+    always @(posedge clk or negedge reset_n) begin
+        if (!reset_n)   begin
+            c_state <= RO;
+            n_state <= RO;
+        end
+        c_state <= n_state;
+    end
+
+    //Part 2: Determine next state using function
+    always @(*) begin
+        case (c_state)
+            RO:      n_state = d_in ? RO : RZ;
+            RZ:      n_state = d_in ? Z2O : RZ;
+            Z2O:     n_state = d_in ? RO : Z2O2Z;
+            Z2O2Z:   n_state = d_in ? Z2O : RZ;
+            default: n_state = RO;
+        endcase
+    end
+
+    //Part 3: Determine ouput
+    always @(*) begin
+        case (c_state)
+            RO:      d_out = d_in ? 0 : 0;
+            RZ:      d_out = d_in ? 0 : 0;
+            Z2O:     d_out = d_in ? 0 : 0;
+            Z2O2Z:   d_out = d_in ? 1 : 0;
+            default: d_out = 1'bX;
+        endcase
+    end
+endmodule
---
+++ W5/SequenceDetector_tb.v
@@ -0,0 +1,58 @@
+`timescale 1ns / 1ns
+
+module SequenceDetector_tb();
+
+parameter  CLOCK_PS  = 10;
+parameter  HCLOCK_PS = 5;
+
+reg  clk;
+reg  reset_n;
+reg  d_in;
+wire d_out;
+
+SequenceDetector top (
+	.clk(clk), .reset_n(reset_n), .d_in(d_in), .d_out(d_out)
+);
+
+// Clock signal generation
+initial begin : CLOCK_GENERATOR
+    clk = 1'b0;
+    forever
+        #HCLOCK_PS clk = ~clk;
+end
+
+
+// Test
+reg [15:0] in_seq   = 16'b0010_0101_0110_1010;  // input sequence
+reg [15:0] ref_res  = 16'b0000_0100_0010_1000;  // reference result
+reg [15:0] test_res = 16'd0;                    // test result
+
+integer i;
+
+initial begin
+    $display("\n:: INITIALIZATION STAGE ::");
+    reset_n = 1'b0;
+	d_in    = 1'b0;
+
+    #HCLOCK_PS;
+    reset_n = 1'b1;
+
+	$display("\n:: TEST ::");
+    d_in = in_seq[0];
+    #CLOCK_PS;
+	for (i = 0; i < 16; i = i + 1) begin
+        test_res[i] = d_out;
+        $display("#%3d \td_out = %b  (%s)", i, d_out, (d_out == ref_res[i]) ? "passed" : "failed");
+		d_in = in_seq[i+1 > 15? 15 : i+1];
+        #CLOCK_PS;
+	end
+    
+	$display("#%3d \ttest %s", i, (test_res == ref_res) ? "passed" : "failed");
+	$display("reference result: %b", ref_res);
+	$display("test result:      %b", test_res);
+
+    $stop;
+end
+
+endmodule
+ 
