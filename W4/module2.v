f02888bd0e8c87ad4b84f52b1a3fced430dc0b1d
Ahnbujang <jhpower901@gmail.com>
2024-11-14 16:55:58 +0900

    W4_last

--- W4/module2.v
+++ W4/module2.v
@@ -1,14 +1,18 @@
 module module2 (
-	input            READ, WRITE,
-	input      [0:7] DIN,
-	input            PIN,
-	output reg [0:7] DOUT,
-	output reg       POUT
+	input        READ, WRITE,
+	input  [0:7] DIN,
+	input        PIN,
+	output [0:7] DOUT,
+	output       POUT
 );
+assign DOUT = ~READ & WRITE ? {DIN[0:1], 1'b1, DIN[3:7]} : DOUT;
+assign POUT = ~READ & WRITE ? PIN : POUT;
+/*
 always @(READ or WRITE) begin
 	if (~READ & WRITE) begin
 		POUT = PIN;
 		DOUT = {DIN[0:1], 1'b1, DIN[3:7]};
 	end
 end
+*/
 endmodule
